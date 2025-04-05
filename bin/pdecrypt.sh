9223bf372d9a160d22a4e1b8b24ddf6183b7450e
Vladimir Chervanev <vlad.chervanev@perchwell.com>
2025-04-05 16:51:00 -0500

    minor

--- bin/pdecrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Decrypt $1 using password (into stdout):"
-read -rs PWD
-openssl aes-256-cbc -d -in $1 -pass env:PWD
-
--- bin/pencrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Encrypt $1 using password (into $1.enc):"
-read -rs PWD
-openssl aes-256-cbc -in $1 -out $1.enc -pass env:PWD
